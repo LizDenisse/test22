dbbbd1806896cfb12664b2ded0724a9f50d955de
Liz <liz.hitches@gmail.com>
2019-08-13 19:43:01 -0400

    trying for mac

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test24.cs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
